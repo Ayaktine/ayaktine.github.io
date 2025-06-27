1140137193713c9df534dda4d809b5c40f89c6df
Ayaktine <95627173+Ayaktine@users.noreply.github.com>
2025-06-27 23:24:14 +0800

    Update sw.js

--- passwordgenerator/sw.js
+++ passwordgenerator/sw.js
@@ -1,22 +1,41 @@
+let CACHE_NAME = 'pwacache-default';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './sw.js',
+  './icon-192.png',
+  './icon-512.png',
+  './version.json'
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('pwacache-v1').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './sw.js',
-        './icon-192.png',
-        './icon-512.png'
-      ]);
-    })
+    fetch('./version.json', { cache: 'no-store' })
+      .then(res => res.json())
+      .then(data => {
+        CACHE_NAME = `pwacache-v${data.version}`;
+        return caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE));
+      })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }))
+    )
+  );
+  clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(resp => {
-      return resp || fetch(event.request);
-    })
+    caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
